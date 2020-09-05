772d01e1fb38d22aa5934f40e1f0810f10340536
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-09-05 20:07:33 +0200

    SAAS-283 Update platform-ui constants

--- packages/platform-ui/src/core/constants.ts
+++ packages/platform-ui/src/core/constants.ts
@@ -2,4 +2,6 @@ export const TERMS_OF_SERVICE_URL = 'https://per.co.na/pmm/platform-terms';
 
 export const PRIVACY_POLICY_URL = 'https://per.co.na/pmm/platform-privacy';
 
+export const PLATFORM_AUTH_API_BASE_URL = 'https://platform-dev.percona.com';
+
 export const PASSWORD_MIN_LENGTH = 10;
