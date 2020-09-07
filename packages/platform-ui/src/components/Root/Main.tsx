27d21bad3683b02c61ed9badc4ba847b23e36c7c
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-09-07 14:37:12 +0200

    SAAS-283 Add toastify container

--- packages/platform-ui/src/components/Root/Main.tsx
+++ packages/platform-ui/src/components/Root/Main.tsx
@@ -1,28 +1,46 @@
 import React, { FC } from 'react';
+import { css } from 'emotion'
 import { Switch, Route } from 'react-router-dom';
 import { PrivateRoute, Authenticated, PublicLayout, PrivateLayout } from 'components';
 import { LoginPage, SignupPage } from 'pages';
 import { LinkButton } from '@grafana/ui';
+import { ToastContainer, Slide } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 export const Main: FC = () => {
   return (
-    <Switch>
-      <PrivateRoute path="/" exact>
-        <Authenticated />
-      </PrivateRoute>
-      <Route path="/login">
-        <LoginPage />
-      </Route>
-      <Route path="/signup">
-        <SignupPage />
-      </Route>
-      <Route path="/logout">
-        <Logout />
-      </Route>
-      <Route path="*">
-        <NotFound />
-      </Route>
-    </Switch>
+    <>
+      <Switch>
+        <PrivateRoute path="/" exact>
+          <Authenticated />
+        </PrivateRoute>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/signup">
+          <SignupPage />
+        </Route>
+        <Route path="/logout">
+          <Logout />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+      <ToastContainer
+        bodyClassName={css`padding: 0.5em;`}
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        transition={Slide}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
   );
 };
 
