a5deca3de3c6b7581919abacc3b6648aabd1fa59
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-09-07 18:58:27 +0200

    SAAS-283 Use .env files

--- packages/platform-ui/src/core/constants.ts
+++ packages/platform-ui/src/core/constants.ts
@@ -4,4 +4,4 @@ export const PRIVACY_POLICY_URL = 'https://per.co.na/pmm/platform-privacy';
 
 export const PASSWORD_MIN_LENGTH = 10;
 
-export const PLATFORM_AUTH_API_BASE_URL = process.env.NODE_ENV === 'development' ? 'https://platform-dev.percona.com' : 'https://platform.percona.com';
+export const PLATFORM_AUTH_API_BASE_URL = process.env.REACT_APP_PLATFORM_AUTH_API_BASE_URL ?? '';
