0ae7e5865526daacbeb4cce9839efa98d9e12a81
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-09-07 18:57:23 +0200

    SAAS-283 Update Authenticated.service.ts

--- packages/platform-ui/src/components/Authenticated/Authenticated.service.ts
+++ packages/platform-ui/src/components/Authenticated/Authenticated.service.ts
@@ -5,9 +5,7 @@ const { AuthPB, AuthGRPC } = apis;
 const { AuthAPIClient } = AuthGRPC;
 const { SignOutRequest } = AuthPB;
 
-type SignOut = () => Promise<apis.AuthPB.SignOutResponse>;
-
-export const signOut: SignOut = () => {
+export const signOut = () => {
   const apiClient = new AuthAPIClient(PLATFORM_AUTH_API_BASE_URL, null, null);
 
   const request = new SignOutRequest();
