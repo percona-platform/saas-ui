1faeaa48f28715c9e66d4baf376b08545ff4e883
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-09-08 16:18:05 +0200

    SAAS-283 Minor fixes

--- packages/platform-ui/src/components/Routes/PrivateRoute.tsx
+++ packages/platform-ui/src/components/Routes/PrivateRoute.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
 import { Route, Redirect, RouteProps } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { AppState } from 'store';
+import { getAuth } from 'store';
 
 // A wrapper for <Route> that redirects to the login if the user is not authenticated.
 export const PrivateRoute: FC<RouteProps> = ({ children, ...rest }) => {
-  const auth = useSelector((state: AppState) => state.auth);
+  const auth = useSelector(getAuth);
+
   return (
     <Route
       {...rest}
--- packages/platform-ui/src/components/Routes/PublicRoute.tsx
+++ packages/platform-ui/src/components/Routes/PublicRoute.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react';
 import { Route, Redirect, RouteProps } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { AppState } from 'store';
+import { getAuth } from 'store';
 
 // A wrapper for <Route> that redirects an authenticated user to the root page if they
 // try to go to the /login route.
 export const PublicRoute: FC<RouteProps> = ({ children, ...rest }) => {
-  const auth = useSelector((state: AppState) => state.auth);
+  const auth = useSelector(getAuth);
 
   return (
     <Route
