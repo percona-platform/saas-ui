9ecb6674d0e6163ab45ccadcea5036308cd479d1
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-09-10 21:48:48 +0200

    SAAS-338 platform-ui: make APIs base path relative

--- packages/platform-ui/src/core/constants.ts
+++ packages/platform-ui/src/core/constants.ts
@@ -4,7 +4,7 @@ export const PRIVACY_POLICY_URL = 'https://per.co.na/pmm/platform-privacy';
 
 export const PASSWORD_MIN_LENGTH = 10;
 
-export const PLATFORM_AUTH_API_BASE_URL = process.env.REACT_APP_PLATFORM_AUTH_API_BASE_URL!;
+export const PLATFORM_AUTH_API_BASE_URL = process.env.REACT_APP_PLATFORM_AUTH_API_BASE_URL;
 
 export const DOWNLOAD_PMM_LINK = 'https://www.percona.com/downloads/pmm2';
 
--- packages/platform-ui/src/react-app-env.d.ts
+++ packages/platform-ui/src/react-app-env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="react-scripts" />
 /// <reference types="@testing-library/jest-dom" />
 /// <reference types="@testing-library/jest-dom/extend-expect" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test';
+    PUBLIC_URL: string;
+    REACT_APP_PLATFORM_AUTH_API_BASE_URL: string;
+  }
+}
