994e3908fff96d9f25bba5fb56450942ceeadf73
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-09-14 12:56:26 +0200

    SAAS-338 Remove types from react-app-env.d.ts

--- packages/platform-ui/src/react-app-env.d.ts
+++ packages/platform-ui/src/react-app-env.d.ts
@@ -1,11 +1,3 @@
 /// <reference types="react-scripts" />
 /// <reference types="@testing-library/jest-dom" />
 /// <reference types="@testing-library/jest-dom/extend-expect" />
-
-declare namespace NodeJS {
-  interface ProcessEnv {
-    NODE_ENV: 'development' | 'production' | 'test';
-    PUBLIC_URL: string;
-    REACT_APP_PLATFORM_BASE_URL: string;
-  }
-}
