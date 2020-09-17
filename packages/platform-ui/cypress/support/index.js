5ab773985dc24a7d6f2acc7bd5ebe2b21bbf68d8
yurkovychv <vasyl.yurkovych@percona.com>
2020-09-17 12:23:04 +0300

    SAAS-340 review fixes

--- packages/platform-ui/cypress/support/index.js
+++ packages/platform-ui/cypress/support/index.js
@@ -14,31 +14,20 @@ setup(config);
 // cypress-testing-library config data-qa attr
 configure({testIdAttribute: 'data-qa'});
 
-// This code is execute after each test in order to add attachments to failed tests in mocha report
+// This code executes after each test in order to add attachments to the tests in mocha report
 Cypress.on('test:after:run', (test, runnable) => {
 
+  // Generating screenshot name with pattern '$Feature -- $TestName (failed).png'
+  // in order to link in to the mocha report
+  // This is the pattern that Cypress uses for screenshot names
   if (test.state === 'failed') {
-    let item = runnable;
-    const nameParts = [runnable.title];
-
-    while (item.parent) {
-      nameParts.unshift(item.parent.title);
-      item = item.parent;
-    }
-    if (runnable.hookName) {
-      nameParts.push(`${runnable.hookName} hook`);
-    }
-
-    // Generating screenshot name with pattern '$Feature -- $TestName (failed).png'
-    // in order to link in to the mocha report
-    // This is the pattern that Cypress uses for screenshot names
-    const fullTestName = nameParts.filter(Boolean).join(' -- ');
-
-    const imageUrl = `screenshots/${Cypress.spec.name}/${fullTestName} (failed).png`;
-
-    const videoUrl = `videos/${Cypress.spec.name}.mp4`;
+    const imageName = `${runnable.parent.title} -- ${runnable.title}`;
+    const imageUrl = `screenshots/${Cypress.spec.name}/${imageName} (failed).png`;
 
     addContext({test}, imageUrl);
-    addContext({test}, videoUrl);
   }
+
+// Generating video name in order to link in to the mocha report
+  const videoUrl = `videos/${Cypress.spec.name}.mp4`;
+  addContext({test}, videoUrl);
 });
