068546b6e81e3abeb765a19bb7eb1f421f7199f1
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-10-01 23:11:50 +0200

    SAAS-363 platform-ui: update core/messages.ts

--- packages/platform-ui/src/core/messages.ts
+++ packages/platform-ui/src/core/messages.ts
@@ -3,4 +3,8 @@ export const Messages = {
   productDescription:
     'Single pane of glass for managing and monitoring the performance of your MySQL, MariaDB, PostgreSQL and MongoDB databases.',
   logoAlt: 'Percona Logo',
+  api: {
+    refreshSessionFailed: 'Unable to refresh user session',
+    unauthenticated: 'Unauthenticated user',
+  },
 };
