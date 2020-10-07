714aed0ed0d45440da1fc9d14c2d11ced7f13698
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-10-07 18:31:00 +0200

    SAAS-363 refactor history

--- packages/platform-ui/src/store/store.ts
+++ packages/platform-ui/src/store/store.ts
@@ -1,17 +1,15 @@
 import { createStore, Store, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import { createBrowserHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 import { AppState } from './types';
 import { loadState } from './persistence';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { authSagas } from './auth/auth.sagas';
 import { createRootReducer } from './reducers';
+import { history } from 'core/history';
 
 const persistedState = loadState();
 const sagaMiddleware = createSagaMiddleware();
-
-export const history = createBrowserHistory();
 
 const middlewares = [sagaMiddleware, routerMiddleware(history)];
 const enhancers = composeWithDevTools(
