49d76a887d9328897633a6af5824c6eb1079e2a7
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-10-09 00:20:49 +0200

    SAAS-363 add tests for persistence sagas

---
+++ packages/platform-ui/src/store/persistence/persistence.sagas.test.ts
@@ -0,0 +1,42 @@
+import { authRefreshAction, authLoginAction, authSignupAction, authLogoutAction } from 'store/auth/auth.reducer';
+import { all, call, takeEvery, select } from 'redux-saga/effects';
+import { save, persistenceSagas } from 'store/persistence/persistence.sagas';
+import * as persistenceEngine from 'store/persistence/engine';
+
+let saveState: jest.SpyInstance;
+
+const TEST_STATE = { test: 'state' };
+
+describe('Persistence Sagas', () => {
+  beforeEach(() => {
+    saveState = jest.spyOn(persistenceEngine, 'saveState');
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test('persistenceSagas', () => {
+    const genObj = persistenceSagas();
+    const expected = all([
+      takeEvery(authRefreshAction.success, save),
+      takeEvery(authRefreshAction.failure, save),
+      takeEvery(authLoginAction.success, save),
+      takeEvery(authLoginAction.failure, save),
+      takeEvery(authSignupAction.success, save),
+      takeEvery(authSignupAction.failure, save),
+      takeEvery(authLogoutAction.success, save),
+      takeEvery(authLogoutAction.failure, save),
+    ]);
+
+    expect(genObj.next().value).toEqual(expected);
+  });
+
+  test('save', () => {
+    const genObj = save();
+
+    expect(genObj.next().value).toEqual(select());
+
+    expect(genObj.next(TEST_STATE).value).toEqual(call(saveState as any, TEST_STATE));
+  });
+});
