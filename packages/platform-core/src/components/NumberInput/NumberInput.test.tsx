5cb40e604e76282cb89156dfb174175a89d9d3ed
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-10-12 17:39:55 +0200

    Squashed commit of the following:
    commit 6e8ea3607979e39f0757532d444970234e744724
    Author: Nicola Lamacchia <nicola.lamacchia@gmail.com>
    Date:   Mon Oct 12 17:36:32 2020 +0200
    
        SAAS-347 fix NumberInput
    
    commit e0b9be6264b2e3cd720e77592400b9962aabcaf1
    Author: Nicola Lamacchia <nicola.lamacchia@gmail.com>
    Date:   Mon Oct 12 17:29:37 2020 +0200
    
        SAAS-347 fix NumberInput
    
    commit 5816ff428b8d3a964cd16032c075faf17262409b
    Author: Nicola Lamacchia <nicola.lamacchia@gmail.com>
    Date:   Mon Oct 12 17:00:10 2020 +0200
    
        SAAS-347 rename NumberInput.tsx to NumberInputField.tsx
    
    commit 87799a7830d93e357ba6825bcb932d84a4ebc134
    Author: Nicola Lamacchia <nicola.lamacchia@gmail.com>
    Date:   Mon Oct 12 16:59:36 2020 +0200
    
        SAAS-347 rename NumberInput.tsx to NumberInputField.tsx
    
    commit 3918f6b785952b3af995793bc20bcee8a063103a
    Merge: e506632 11b5b61
    Author: Nicola Lamacchia <nicola.lamacchia@gmail.com>
    Date:   Mon Oct 12 16:50:19 2020 +0200
    
        Merge branch 'main' into SAAS-347-numeric-input-component
    
    commit e506632fd40e0d7698c413aa385b88fdf487e9cb
    Author: Nicola Lamacchia <nicola.lamacchia@gmail.com>
    Date:   Mon Oct 12 14:21:11 2020 +0200
    
        SAAS-347 update tests for NumberInput
    
    commit 407aba575c6fa83edfd607db01293b574eaaf191
    Merge: 77ffce6 940e25b
    Author: Nicola Lamacchia <nicola.lamacchia@gmail.com>
    Date:   Mon Oct 12 13:41:51 2020 +0200
    
        Merge branch 'main' into SAAS-347-numeric-input-component
    
    commit 77ffce6a3966fe3580a8f433ec83d6dcb10ac8f9
    Author: Nicola Lamacchia <nicola.lamacchia@gmail.com>
    Date:   Mon Oct 12 11:30:39 2020 +0200
    
        SAAS-347 platform-core: add a coverage npm task
    
    commit 570b8ea19fa536a7f86dea3352abd0aab8ce8eaa
    Author: Nicola Lamacchia <nicola.lamacchia@gmail.com>
    Date:   Fri Oct 9 19:23:36 2020 +0200
    
        SAAS-347 platform-core: update components' index
    
    commit e2def5ba56ab0bf992e2bd1cec308d55898dd879
    Author: Nicola Lamacchia <nicola.lamacchia@gmail.com>
    Date:   Fri Oct 9 19:22:52 2020 +0200
    
        SAAS-347 platform-core: add NumberInput

---
+++ packages/platform-core/src/components/NumberInput/NumberInput.test.tsx
@@ -0,0 +1,173 @@
+import React, { FC } from 'react';
+import { mount } from 'enzyme';
+import { NumberInputField } from './NumberInputField';
+import { Form, Field } from 'react-final-form';
+
+jest.spyOn(console, 'error').mockImplementation(() => {});
+
+const Wrapper: FC = ({ children }) => (
+  <Form onSubmit={() => {}}>
+    {() => (
+      <form>{children}</form>
+    )}
+  </Form>
+);
+
+describe('NumberInputField::', () => {
+  it('should render an input element and two buttons', () => {
+    const wrapper = mount(<Wrapper><NumberInputField name="test" /></Wrapper>);
+
+    const field = wrapper.find(Field);
+
+    expect(field).toHaveLength(1);
+    expect(wrapper.find('input')).toHaveLength(1);
+    expect(wrapper.find('button')).toHaveLength(2);
+
+    wrapper.unmount();
+  });
+
+  it('should call passed validators', () => {
+    const validatorOne = jest.fn();
+    const validatorTwo = jest.fn();
+    const wrapper = mount(<Wrapper><NumberInputField name="test" validators={[validatorOne, validatorTwo]} /></Wrapper>);
+
+    expect(validatorOne).toBeCalledTimes(1);
+    expect(validatorTwo).toBeCalledTimes(1);
+
+    wrapper.unmount();
+  });
+
+  it('should show an error on invalid input', () => {
+    const validatorOne = jest.fn().mockReturnValue('some error');
+    const validatorTwo = jest.fn();
+    const wrapper = mount(<Wrapper><NumberInputField name="test" validators={[validatorOne, validatorTwo]} /></Wrapper>);
+
+    expect(wrapper.find('[data-qa="test-field-error-message"]').text()).toBe('');
+
+    expect(validatorOne).toBeCalledTimes(1);
+
+    wrapper.find('input').at(0).simulate('change', { target: { value: 'Test' } });
+
+    expect(validatorOne).toBeCalledTimes(2);
+    expect(validatorTwo).toBeCalledTimes(0);
+
+    expect(wrapper.find('[data-qa="test-field-error-message"]').text()).toBe('some error');
+
+    wrapper.unmount();
+  });
+
+  it('should show validation errors on blur if specified', () => {
+    const validatorOne = jest.fn();
+    const validatorTwo = jest.fn().mockReturnValue('some error');
+    const wrapper = mount(<Wrapper><NumberInputField showErrorOnBlur name="test" validators={[validatorOne, validatorTwo]} /></Wrapper>);
+
+    wrapper.find('input').at(0).simulate('change', { target: { value: 'Test' } });
+
+    expect(validatorOne).toBeCalledTimes(2);
+    expect(validatorTwo).toBeCalledTimes(2);
+
+    expect(wrapper.find('[data-qa="test-field-error-message"]').text()).toBe('');
+
+    wrapper.find('input').at(0).simulate('blur');
+
+    expect(wrapper.find('[data-qa="test-field-error-message"]').text()).toBe('some error');
+
+    wrapper.unmount();
+  });
+
+  it('should show no labels if one is not specified', () => {
+    const wrapper = mount(<Wrapper><NumberInputField name="test" /></Wrapper>);
+
+    expect(wrapper.find('[data-qa="test-field-label"]').length).toBe(0);
+
+    wrapper.unmount();
+  });
+
+  it('should show a label if one is specified', () => {
+    const wrapper = mount(<Wrapper><NumberInputField label="test label" name="test" /></Wrapper>);
+
+    expect(wrapper.find('[data-qa="test-field-label"]').length).toBe(1);
+    expect(wrapper.find('[data-qa="test-field-label"]').text()).toBe('test label');
+
+    wrapper.unmount();
+  });
+
+  it('should show an asterisk on the label if the field is required', () => {
+    const wrapper = mount(<Wrapper><NumberInputField label="test label" name="test" required /></Wrapper>);
+
+    expect(wrapper.find('[data-qa="test-field-label"]').length).toBe(1);
+    expect(wrapper.find('[data-qa="test-field-label"]').text()).toBe('test label *');
+
+    wrapper.unmount();
+  });
+
+  it('should hide arrow buttons when disabled', () => {
+    const wrapper = mount(<Wrapper><NumberInputField name="test" disabled /></Wrapper>);
+
+    expect(wrapper.find('input')).toHaveLength(1);
+    expect(wrapper.find('button')).toHaveLength(0);
+
+    wrapper.unmount();
+  });
+
+  it('should apply the passed class name to the inner input element', () => {
+    const wrapper = mount(<Wrapper><NumberInputField name="test" className="testClass" /></Wrapper>);
+
+    expect(wrapper.find('input').hasClass('testClass')).toBe(true);
+
+    wrapper.unmount();
+  });
+
+  it('should change the value when clicking on the arrow buttons', () => {
+    const wrapper = mount(<Wrapper><NumberInputField name="test" className="testClass" /></Wrapper>);
+
+    const mockedStepUp = jest.fn();
+    const mockedStepDown = jest.fn();
+
+    (wrapper.find('input').instance() as any).stepUp = mockedStepUp;
+    (wrapper.find('input').instance() as any).stepDown = mockedStepDown;
+
+    expect(mockedStepUp).toBeCalledTimes(0);
+    expect(mockedStepDown).toBeCalledTimes(0);
+
+    wrapper.find('button').at(0).simulate('click');
+
+    expect(mockedStepUp).toBeCalledTimes(1);
+    expect(mockedStepDown).toBeCalledTimes(0);
+
+    wrapper.find('button').at(1).simulate('click');
+
+    expect(mockedStepUp).toBeCalledTimes(1);
+    expect(mockedStepDown).toBeCalledTimes(1);
+
+    wrapper.unmount();
+  });
+
+  it('should trigger a change event when clicking on arrow buttons', () => {
+    const mockedStepUp = jest.fn();
+    const mockedStepDown = jest.fn();
+    const mockedDispatchEvent = jest.fn();
+
+    const wrapper = mount(<Wrapper><NumberInputField name="test" className="testClass" /></Wrapper>);
+
+    (wrapper.find('input').instance() as any).stepUp = mockedStepUp;
+    (wrapper.find('input').instance() as any).stepDown = mockedStepDown;
+    (wrapper.find('input').instance() as any).dispatchEvent = mockedDispatchEvent;
+
+    expect(mockedDispatchEvent).toBeCalledTimes(0);
+
+    wrapper.find('button').at(0).simulate('click');
+
+    expect(mockedDispatchEvent).toBeCalledTimes(1);
+    expect(mockedDispatchEvent.mock.calls[0][0].type).toEqual('change');
+    expect(mockedDispatchEvent.mock.calls[0][0].bubbles).toBe(true);
+
+    wrapper.find('button').at(1).simulate('click');
+
+    expect(mockedDispatchEvent).toBeCalledTimes(2);
+    expect(mockedDispatchEvent.mock.calls[1][0].type).toEqual('change');
+    expect(mockedDispatchEvent.mock.calls[1][0].bubbles).toBe(true);
+
+    wrapper.unmount();
+  });
+});
---
+++ packages/platform-core/src/components/NumberInput/NumberInputField.tsx
@@ -0,0 +1,116 @@
+import React, { FC, useCallback, useRef, useMemo } from 'react';
+import { cx } from 'emotion';
+import { useTheme } from '@grafana/ui';
+import { getStyles } from './NumberInput.styles';
+import { Field, FieldMetaState, FieldInputProps, UseFieldConfig } from 'react-final-form';
+import { Validator, compose } from '../../shared/validators';
+
+export interface NumberInputFieldProps extends UseFieldConfig<number> {
+  label?: string;
+  className?: string;
+  disabled?: boolean;
+  fieldClassName?: string;
+  name: string;
+  onChange?: (value: string) => undefined;
+  placeholder?: string;
+  required?: boolean;
+  showErrorOnBlur?: boolean;
+  validators?: Validator[];
+}
+
+interface NumberFieldRenderProps {
+  input: FieldInputProps<number>;
+  meta: FieldMetaState<number>;
+}
+
+export const NumberInputField: FC<NumberInputFieldProps> = React.memo(({
+  showErrorOnBlur = false,
+  className,
+  disabled = false,
+  fieldClassName,
+  label,
+  name,
+  placeholder,
+  required = false,
+  validators,
+  ...fieldConfig
+}) => {
+  const theme = useTheme();
+  const styles = useMemo(() => getStyles(theme), [theme]);
+  const inputId = `input-${name}-id`;
+  const validate = useMemo(() => (Array.isArray(validators) ? compose(...validators) : undefined), [
+    validators,
+  ]);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const dispatchChangeEvent = useCallback(() => {
+    const event = new Event('change', { bubbles: true });
+
+    inputRef.current!.dispatchEvent(event);
+  }, [inputRef]);
+
+  const stepUp = () => {
+    inputRef.current!.stepUp();
+    dispatchChangeEvent();
+  };
+
+  const stepDown = () => {
+    inputRef.current!.stepDown();
+    dispatchChangeEvent();
+  };
+
+  return (
+    <Field {...fieldConfig} name={name} validate={validate}>
+      {({ input, meta }: NumberFieldRenderProps) => {
+        const validationError = ((!showErrorOnBlur && meta.modified) || meta.touched) && meta.error;
+
+        return (
+          <div className={cx(styles.field, fieldClassName)} data-qa={`${name}-field-container`}>
+            {label && (
+              <label className={styles.label} htmlFor={inputId} data-qa={`${name}-field-label`}>{`${label}${required ? ' *' : ''}`}
+              </label>
+            )}
+            <span className={styles.inputWrapper}>
+              <input
+                id={inputId}
+                type="number"
+                {...input}
+                ref={inputRef}
+                disabled={disabled}
+                placeholder={placeholder}
+                data-qa={`${name}-text-input`}
+                className={cx(styles.input, { invalid: !!validationError }, className)}
+              />
+              {!disabled && (
+                <>
+                  <button
+                    type="button"
+                    className={styles.buttonUp}
+                    onClick={stepUp}
+                    disabled={disabled}
+                  >
+                    <span className={styles.arrowUp} />
+                  </button>
+                  <button
+                    type="button"
+                    className={styles.buttonDown}
+                    onClick={stepDown}
+                    disabled={disabled}
+                  >
+                    <span className={styles.arrowDown} />
+                  </button>
+                </>
+              )}
+            </span>
+            <div data-qa={`${name}-field-error-message`} className={styles.errorMessage}>
+              {validationError}
+            </div>
+          </div>
+        );
+      }}
+    </Field>
+  );
+});
+
+NumberInputField.displayName = 'NumberInputField';
