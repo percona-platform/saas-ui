d594382dfe6140e670911d6d15b3aad723eb2e85
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-10-14 16:41:21 +0200

    SAAS-362 update tests for Signup

--- packages/platform-ui/src/pages/Signup/Signup.test.tsx
+++ packages/platform-ui/src/pages/Signup/Signup.test.tsx
@@ -4,10 +4,13 @@ import { act } from 'react-dom/test-utils';
 import { fireEvent } from '@testing-library/react';
 import { TestContainer } from 'components/TestContainer';
 import { SignupPage } from './Signup';
+import * as authApi from 'core/api/auth';
+
+jest.spyOn(authApi, 'signUp');
 
 let container: HTMLElement;
 
-describe('Platform login::', () => {
+describe('Platform Sign up', () => {
   beforeEach(() => {
     container = document.createElement('div');
     document.body.appendChild(container);
@@ -18,12 +21,12 @@ describe('Platform login::', () => {
     container.remove();
   });
 
-  test('has login button disabled at start-up', () => {
+  test('has sign-up button disabled at start-up', () => {
     act(() => {
       render(<TestContainer><SignupPage /></TestContainer>, container);
     });
 
-    expect(container.querySelector('[data-qa="login-submit-button"]')?.hasAttribute('disabled')).toBe(true);
+    expect(container.querySelector('[data-qa="signup-submit-button"]')?.hasAttribute('disabled')).toBe(true);
   });
 
   test('submit button is enabled after valid input in form fields', () => {
@@ -39,6 +42,27 @@ describe('Platform login::', () => {
     fireEvent.change(passwordInput!, { target: { value: 'FooBar123!!!' } });
     fireEvent.click(checkbox!);
 
-    expect(container.querySelector('[data-qa="login-submit-button"]')?.hasAttribute('disabled')).toBe(false);
+    expect(container.querySelector('[data-qa="signup-submit-button"]')?.hasAttribute('disabled')).toBe(false);
+  });
+
+  test('calls the sign up api on sign up button click', async () => {
+    act(() => {
+      render(<TestContainer><SignupPage /></TestContainer>, container);
+    });
+
+    const emailInput = container.querySelector('[data-qa="email-text-input"]');
+    const passwordInput = container.querySelector('[data-qa="password-password-input"]');
+    const checkbox = container.querySelector('[data-qa="consent-checkbox-input"]');
+    const logoutButton = container.querySelector('[data-qa="signup-submit-button"]');
+
+    fireEvent.change(emailInput!, { target: { value: 'test@test.test' } });
+    fireEvent.change(passwordInput!, { target: { value: 'FooBar123!!!' } });
+    fireEvent.click(checkbox!);
+
+    await act(async () => {
+      fireEvent.click(logoutButton!);
+    });
+
+    expect(authApi.signUp).toBeCalledTimes(1);
   });
 });
