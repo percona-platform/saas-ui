f2b1076c283249e8a1335dd781507702b02251da
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-10-26 11:20:17 +0100

    SAAS-405 use the dataQa helper in Dropdown tests

--- packages/platform-core/src/components/Dropdown/Dropdown.test.tsx
+++ packages/platform-core/src/components/Dropdown/Dropdown.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import { fireEvent } from '@testing-library/react';
+import { dataQa } from 'shared';
 import { Dropdown } from './Dropdown';
 
 let container: HTMLElement;
@@ -28,7 +29,7 @@ describe('Dropdown ::', () => {
       render(<Dropdown toggle={Toggle}><a href="/">root</a><a href="/test">test</a></Dropdown>, container);
     });
 
-    expect(container.querySelector('[data-qa="dropdown-toggle"]')).not.toBe(null);
+    expect(container.querySelector(dataQa('dropdown-toggle'))).not.toBe(null);
   });
 
   test('clicking on the toggle toggles the menu visibility', async () => {
@@ -36,21 +37,21 @@ describe('Dropdown ::', () => {
       render(<Dropdown toggle={Toggle}><a href="/">root</a><a href="/test">test</a></Dropdown>, container);
     });
 
-    const toggle = container.querySelector('[data-qa="dropdown-toggle"]');
+    const toggle = container.querySelector(dataQa('dropdown-toggle'));
 
-    expect(container.querySelector('[data-qa="dropdown-menu"]')).toBe(null);
-
-    await act(async () => {
-      fireEvent.click(toggle!);
-    });
-
-    expect(container.querySelector('[data-qa="dropdown-menu"]')).not.toBe(null);
+    expect(container.querySelector(dataQa('dropdown-menu'))).toBe(null);
 
     await act(async () => {
       fireEvent.click(toggle!);
     });
 
-    expect(container.querySelector('[data-qa="dropdown-menu"]')).toBe(null);
+    expect(container.querySelector(dataQa('dropdown-menu'))).not.toBe(null);
+
+    await act(async () => {
+      fireEvent.click(toggle!);
+    });
+
+    expect(container.querySelector(dataQa('dropdown-menu'))).toBe(null);
   });
 
   test('clicking outside the dropdown closes the menu', async () => {
@@ -58,19 +59,19 @@ describe('Dropdown ::', () => {
       render(<Dropdown toggle={Toggle}><a href="/">root</a><a href="/test">test</a></Dropdown>, container);
     });
 
-    const toggle = container.querySelector('[data-qa="dropdown-toggle"]');
+    const toggle = container.querySelector(dataQa('dropdown-toggle'));
 
     await act(async () => {
       fireEvent.click(toggle!);
     });
 
-    expect(container.querySelector('[data-qa="dropdown-menu"]')).not.toBe(null);
+    expect(container.querySelector(dataQa('dropdown-menu'))).not.toBe(null);
 
     await act(async () => {
       fireEvent.mouseDown(container);
     });
 
-    expect(container.querySelector('[data-qa="dropdown-menu"]')).toBe(null);
+    expect(container.querySelector(dataQa('dropdown-menu'))).toBe(null);
   });
 
   test('mousedown on the dropdown does not close the menu', async () => {
@@ -83,7 +84,7 @@ describe('Dropdown ::', () => {
       </Dropdown>, container);
     });
 
-    const toggle = container.querySelector('[data-qa="dropdown-toggle"]');
+    const toggle = container.querySelector(dataQa('dropdown-toggle'));
 
     await act(async () => {
       fireEvent.click(toggle!);
@@ -92,10 +93,10 @@ describe('Dropdown ::', () => {
     // NOTE: this needs to be in a separate 'act'
     await act(async () => {
       fireEvent.mouseDown(toggle!);
-      fireEvent.mouseDown(container.querySelector('[data-qa="dropdown-menu"]')!);
+      fireEvent.mouseDown(container.querySelector(dataQa('dropdown-menu'))!);
     });
 
-    expect(container.querySelector('[data-qa="dropdown-menu"]')).not.toBe(null);
+    expect(container.querySelector(dataQa('dropdown-menu'))).not.toBe(null);
   });
 
   test('clicking on a menu item propagates the event and closes the menu', async () => {
@@ -108,7 +109,7 @@ describe('Dropdown ::', () => {
       </Dropdown>, container);
     });
 
-    const toggle = container.querySelector('[data-qa="dropdown-toggle"]');
+    const toggle = container.querySelector(dataQa('dropdown-toggle'));
 
     expect(menuAction).toBeCalledTimes(0);
 
@@ -118,7 +119,7 @@ describe('Dropdown ::', () => {
 
     // NOTE: this needs to be in a separate 'act'
     await act(async () => {
-      const menuItem = container.querySelector('[data-qa="menu-item"]');
+      const menuItem = container.querySelector(dataQa('menu-item'));
 
       fireEvent.click(menuItem!);
     });
