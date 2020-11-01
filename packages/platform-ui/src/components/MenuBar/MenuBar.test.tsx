b68ff81d8f4a5436ff95cf897f72c576cb7866d7
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-11-01 21:08:06 +0100

    SAAS-404 add MenuBar tests

---
+++ packages/platform-ui/src/components/MenuBar/MenuBar.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { TestContainer } from 'components/TestContainer';
+import { fireEvent } from '@testing-library/react';
+import * as authApi from 'core/api/auth';
+import { dataQa } from '@percona/platform-core';
+import { AuthPB } from 'core';
+import { MenuBar } from './MenuBar';
+
+const { SignOutResponse } = AuthPB;
+
+jest.spyOn(authApi, 'signOut').mockImplementation(() => Promise.resolve({} as AuthPB.SignOutResponse));
+
+let container: HTMLElement;
+
+// const clickOnDataQa = (dataQaAttr: string) => {
+//   fireEvent.click(container.querySelector(dataQa(dataQaAttr))!);
+// };
+
+describe('MenuBar', () => {
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+  });
+
+  test('clicking on the profile logout button calls the logout API', async () => {
+    act(() => {
+      render(<TestContainer><MenuBar /></TestContainer>, container);
+    });
+
+    act(() => {
+      fireEvent.click(container.querySelector(dataQa('menu-bar-profile-dropdown-toggle'))!);
+    });
+
+    await act(async () => {
+      fireEvent.click(container.querySelector(dataQa('menu-bar-profile-dropdown-logout'))!);
+    });
+
+    expect(authApi.signOut).toBeCalledTimes(1);
+  });
+});
