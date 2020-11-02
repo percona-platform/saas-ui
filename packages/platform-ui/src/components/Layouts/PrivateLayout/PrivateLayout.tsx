d8e8ef244910b6b5b829e359c9b106794921c813
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-11-02 16:47:11 +0100

    SAAS-404 remove comment

--- packages/platform-ui/src/components/Layouts/PrivateLayout/PrivateLayout.tsx
+++ packages/platform-ui/src/components/Layouts/PrivateLayout/PrivateLayout.tsx
@@ -6,7 +6,6 @@ import { getStyles } from './PrivateLayout.styles';
 export const PrivateLayout: FC = ({ children }) => {
   const styles = useStyles(getStyles);
 
-  // TODO: this will have to be modified according to the future design
   return (
     <main className={styles.main}>
       <MenuBar />
