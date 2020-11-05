3ceaec4f712a7409f53b2309ab54204efbc36ed6
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-11-05 16:43:11 +0100

    SAAS-432 update Authenticated.test.tsx

--- packages/platform-ui/src/components/Authenticated/Authenticated.test.tsx
+++ packages/platform-ui/src/components/Authenticated/Authenticated.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import { TestContainer } from 'components/TestContainer';
-import { fireEvent } from '@testing-library/react';
 import * as authApi from 'core/api/auth';
 import { Authenticated } from './Authenticated';
 
@@ -29,31 +28,11 @@ describe('Authenticated Page', () => {
     container.remove();
   });
 
-  test('has logout button enabled at start-up', () => {
-    act(() => {
-      render(<TestContainer><Authenticated /></TestContainer>, container);
-    });
-
-    expect(container.querySelector('[data-qa="logout-action-button"]')?.hasAttribute('disabled')).toBe(false);
-  });
-
-  test('shows the email of the authenticated user', () => {
-    act(() => {
+  test('shows the email of the authenticated user', async () => {
+    await act(async () => {
       render(<TestContainer><Authenticated /></TestContainer>, container);
     });
 
     expect(container.querySelector('[data-qa="user-email"]')?.textContent).toEqual('test@test.test');
-  });
-
-  test('calls the logout api on logout button click', () => {
-    act(() => {
-      render(<TestContainer><Authenticated /></TestContainer>, container);
-    });
-
-    const logoutButton = container.querySelector('[data-qa="logout-action-button"]');
-
-    fireEvent.click(logoutButton!);
-
-    expect(authApi.signOut).toBeCalledTimes(1);
   });
 });
