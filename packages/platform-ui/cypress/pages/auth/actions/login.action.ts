ed8baac71dfcd3e0802bf7a59e13bec0f6e51a30
yurkovychv <vasyl.yurkovych@percona.com>
2020-11-18 18:14:29 +0200

    SAAS-374 removed unnecessary logs

--- packages/platform-ui/cypress/pages/auth/actions/login.action.ts
+++ packages/platform-ui/cypress/pages/auth/actions/login.action.ts
@@ -1,21 +1,12 @@
 /// <reference types="cypress" />
 import { User } from 'pages/common/interfaces/Auth';
-import { authLoginAction } from '../../../../src/store/auth';
 import { INVALID_USER } from 'pages/common/constants';
+import { authLoginAction } from '../../../../src/store/auth';
 
 export const runLoginAction = (user: User) => {
   cy.window().its('store')
     .invoke('dispatch', authLoginAction.request(user));
-
-  cy.log(user.toString());
-
-  if (user === INVALID_USER.user) {
-    cy.log('GOT INVALID USER EXITING ...');
-
-    return;
-  }
-
-  cy.log('moving ...');
+  if (user === INVALID_USER.user) return;
 
   cy.wait('@signin').then(() => {
     cy.visit('/');
