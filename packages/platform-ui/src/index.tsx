4b7324fd5bc4b2f6770e413c8a3d68b27ff23f60
yurkovychv <vasyl.yurkovych@percona.com>
2020-11-18 12:10:47 +0200

    SAAS-374 exposing store for Cypress

--- packages/platform-ui/src/index.tsx
+++ packages/platform-ui/src/index.tsx
@@ -57,3 +57,12 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// expose store during tests
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+if (window.Cypress) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  window.store = store;
+}
