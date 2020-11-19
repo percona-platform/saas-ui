328800fe4db4b64c09ad94990c2891200a13d8fb
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2020-11-19 16:52:14 +0100

    PMM-6925 fix test for Icon

--- packages/platform-core/src/components/Icon/Icon.test.tsx
+++ packages/platform-core/src/components/Icon/Icon.test.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { Icon } from './Icon';
-import { TableToolbarAdd } from '../../shared/icons';
+import { PlusSquare } from '../../shared/icons';
 
 describe('Icon::', () => {
   it('should display the correct icon', () => {
-    const wrapper = mount(<Icon name="toolbarAdd" />);
+    const wrapper = mount(<Icon name="plusSquare" />);
 
-    expect(wrapper.find(TableToolbarAdd).length).toEqual(1);
+    expect(wrapper.find(PlusSquare).length).toEqual(1);
 
     wrapper.unmount();
   });
