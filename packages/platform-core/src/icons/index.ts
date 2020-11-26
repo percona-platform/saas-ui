f65780fbd2b4ef06ecc874d6e13368b4e266cd66
Alex Tymchuk <alexander.tymchuk@percona.com>
2020-11-27 02:18:00 +0300

    SAAS-403 better svg icon markup

--- packages/platform-core/src/icons/index.ts
+++ packages/platform-core/src/icons/index.ts
@@ -1,2 +1,7 @@
-export * from './ThemeDark';
-export * from './ThemeLight';
+import { ThemeDark } from './ThemeDark';
+import { ThemeLight } from './ThemeLight';
+
+export const Icons = {
+  ThemeDark,
+  ThemeLight,
+};
