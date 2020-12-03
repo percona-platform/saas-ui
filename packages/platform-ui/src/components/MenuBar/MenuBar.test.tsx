92dc096733268ed38e3d7437fdc6d1b4f379963b
Alex Tymchuk <alexander.tymchuk@percona.com>
2020-12-03 16:47:58 +0300

    SAAS-403 fix the MenuBar unit test

--- packages/platform-ui/src/components/MenuBar/MenuBar.test.tsx
+++ packages/platform-ui/src/components/MenuBar/MenuBar.test.tsx
@@ -3,22 +3,37 @@ import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import { TestContainer } from 'components/TestContainer';
 import { fireEvent } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 import * as authApi from 'core/api/auth';
 import { dataQa } from '@percona/platform-core';
 import { AuthPB } from 'core';
 import { MenuBar } from './MenuBar';
 
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useSelector: jest.fn(),
+}));
+
 jest.spyOn(authApi, 'signOut').mockImplementation(() => Promise.resolve({} as AuthPB.SignOutResponse));
 
 let container: HTMLElement;
+const mockAppState = {
+  auth: {
+    authenticated: true,
+  },
+};
 
 describe('MenuBar', () => {
   beforeEach(() => {
+    useSelector.mockImplementation((callback) => {
+      return callback(mockAppState);
+    });
     container = document.createElement('div');
     document.body.appendChild(container);
   });
 
   afterEach(() => {
+    useSelector.mockClear();
     unmountComponentAtNode(container);
     container.remove();
   });
