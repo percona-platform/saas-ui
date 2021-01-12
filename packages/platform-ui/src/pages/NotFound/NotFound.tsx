4fab392b3d9fcfea6a2e9e226bc4abfc8226e684
Fabio Silva <ffjs1993@gmail.com>
2021-01-12 12:06:56 +0000

    SAAS-385 Add home link to 404 button

--- packages/platform-ui/src/pages/NotFound/NotFound.tsx
+++ packages/platform-ui/src/pages/NotFound/NotFound.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react';
 import { useSelector } from 'react-redux';
 import { useStyles, Button } from '@grafana/ui';
+import { Link } from 'react-router-dom';
+import { Routes } from 'core/routes';
 import { getCurrentTheme } from 'store/theme';
 import whiteLogo from 'assets/404-light.svg';
 import darkLogo from 'assets/404-dark.svg';
@@ -14,7 +16,9 @@ export const NotFound: FC = () => {
   return (
     <div className={styles.contentWrapper}>
       <img data-qa="404-image" className={styles.logo} alt="404" src={isDark ? darkLogo : whiteLogo} />
-      <Button data-qa="404-home-button" className={styles.homeButton}>{Messages.homepage}</Button>
+      <Button data-qa="404-home-button" className={styles.homeButton}>
+        <Link to={Routes.root}>{Messages.homepage}</Link>
+      </Button>
     </div>
   );
 };
