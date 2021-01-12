497bb9a184dcee3e4662e48b3a5a63d33953b3d3
Fabio Silva <ffjs1993@gmail.com>
2021-01-12 22:29:17 +0000

    SAAS-385 Dynamic data-qa on NotFound

--- packages/platform-ui/src/pages/NotFound/NotFound.tsx
+++ packages/platform-ui/src/pages/NotFound/NotFound.tsx
@@ -4,7 +4,7 @@ import { useStyles, Button } from '@grafana/ui';
 import { Link } from 'react-router-dom';
 import { Routes } from 'core/routes';
 import { getCurrentTheme } from 'store/theme';
-import whiteLogo from 'assets/404-light.svg';
+import lightLogo from 'assets/404-light.svg';
 import darkLogo from 'assets/404-dark.svg';
 import { getStyles } from './NotFound.styles';
 import { Messages } from './NotFound.messages';
@@ -12,10 +12,12 @@ import { Messages } from './NotFound.messages';
 export const NotFound: FC = () => {
   const { isDark } = useSelector(getCurrentTheme);
   const styles = useStyles(getStyles);
+  const dataQaImgAttr = isDark ? 'dark' : 'light';
+  const logo = isDark ? darkLogo : lightLogo;
 
   return (
     <div className={styles.contentWrapper}>
-      <img data-qa="404-image" className={styles.logo} alt="404" src={isDark ? darkLogo : whiteLogo} />
+      <img data-qa={`404-image-${dataQaImgAttr}`} className={styles.logo} alt="404" src={logo} />
       <Button data-qa="404-home-button" className={styles.homeButton}>
         <Link to={Routes.root}>{Messages.homepage}</Link>
       </Button>
