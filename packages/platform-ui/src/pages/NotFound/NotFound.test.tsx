51979d4ba3d73021cf9c1e25dd1f3a9cc96bf398
Fabio Silva <ffjs1993@gmail.com>
2021-01-12 23:25:35 +0000

    SAAS-385 Add tests to NotFound

---
+++ packages/platform-ui/src/pages/NotFound/NotFound.test.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { fireEvent } from '@testing-library/react';
+import { GrafanaTheme } from '@grafana/data';
+import { history } from 'core/history';
+import { Routes } from 'core/routes';
+import * as themeSelectors from 'store/theme/theme.selectors';
+import { TestContainer } from 'components/TestContainer';
+import { NotFound } from './NotFound';
+
+let container: HTMLElement;
+const getTheme = jest.spyOn(themeSelectors, 'getCurrentTheme');
+
+describe('NotFound', () => {
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    const mockTheme: Partial<GrafanaTheme> = { isDark: true };
+
+    getTheme.mockImplementation(() => mockTheme as GrafanaTheme);
+  });
+
+  afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    jest.clearAllMocks();
+  });
+
+  test('sets correct dark image', () => {
+    act(() => {
+      render(<TestContainer><NotFound /></TestContainer>, container);
+    });
+
+    expect(container.querySelector('[data-qa="404-image-dark"]')).not.toBeNull();
+  });
+
+  test('sets correct light image', () => {
+    getTheme.mockImplementation(() => ({ isDark: false }) as GrafanaTheme);
+    act(() => {
+      render(<TestContainer><NotFound /></TestContainer>, container);
+    });
+
+    expect(container.querySelector('[data-qa="404-image-light"]')).not.toBeNull();
+  });
+
+  test('links to root', () => {
+    act(() => {
+      render(<TestContainer><NotFound /></TestContainer>, container);
+    });
+
+    const button = container.querySelector('[data-qa="404-home-button"]');
+
+    if (button) {
+      fireEvent.click(button);
+    }
+
+    expect(history.location.pathname).toEqual(Routes.root);
+  });
+});
