74cad0927360c8a4db9f4634533b9d0526f243da
Fabio Silva <ffjs1993@gmail.com>
2021-01-14 21:56:48 +0000

    SAAS-385 NotFound: remove args destructure

--- packages/platform-ui/src/pages/NotFound/NotFound.styles.ts
+++ packages/platform-ui/src/pages/NotFound/NotFound.styles.ts
@@ -1,41 +1,30 @@
 import { css } from 'emotion';
 import { GrafanaTheme } from '@grafana/data';
 
-export const getStyles = (theme: GrafanaTheme) => {
-  const {
-    colors: {
-      dashboardBg,
-    },
-    spacing: {
-      lg: spacingLg,
-    },
-  } = theme;
-
-  return {
-    contentWrapper: css`
+export const getStyles = ({ colors, spacing }: GrafanaTheme) => ({
+  contentWrapper: css`
       display: flex;
       flex-direction: column;
       justify-content: center;
       min-height: 100vh;
-      background-color: ${dashboardBg};
+      background-color: ${colors.dashboardBg};
       align-items: center;
       // TODO set this on core and change everywhere, as it's used several times
       padding: 2em;
     `,
-    homeButton: css`
+  homeButton: css`
       width: 180px;
       justify-content: center;
-      margin-top: ${spacingLg};
+      margin-top: ${spacing.lg};
 
       a {
         color: inherit;
         text-decoration: none;
       }
     `,
-    logo: css`
+  logo: css`
       width: 100%;
       min-width: 250px;
       max-width: 600px;
     `,
-  };
-};
+});
