2220b79ed6039f4808b0f0130dcafcc9e9165de6
Fabio Silva <ffjs1993@gmail.com>
2021-01-14 22:19:36 +0000

    SAAS-385 NotFound: remove useless mocks

--- packages/platform-ui/src/pages/NotFound/NotFound.test.tsx
+++ packages/platform-ui/src/pages/NotFound/NotFound.test.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { GrafanaTheme } from '@grafana/data';
 import { dataQa } from '@percona/platform-core';
 import { Routes } from 'core/routes';
-import * as themeSelectors from 'store/theme/theme.selectors';
 import { TestContainer } from 'components/TestContainer';
 import { NotFound } from './NotFound';
 
-const getTheme = jest.spyOn(themeSelectors, 'getCurrentTheme');
-
 describe('NotFound', () => {
-  beforeEach(() => {
-    const mockTheme: Partial<GrafanaTheme> = { isDark: true };
-
-    getTheme.mockImplementation(() => mockTheme as GrafanaTheme);
-  });
-
-  afterEach(() => {
-    getTheme.mockClear();
-  });
-
   test('has the 404 image', () => {
     const { container } = render(<TestContainer><NotFound /></TestContainer>);
 
