482c83820748b1f382908cce735cf98e3287afb0
Fabio Silva <ffjs1993@gmail.com>
2021-02-09 18:09:11 +0000

    SAAS-385 fixed space indentation

--- packages/platform-ui/src/pages/NotFound/NotFound.styles.ts
+++ packages/platform-ui/src/pages/NotFound/NotFound.styles.ts
@@ -3,27 +3,27 @@ import { GrafanaTheme } from '@grafana/data';
 
 export const getStyles = ({ colors, spacing }: GrafanaTheme) => ({
   contentWrapper: css`
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      min-height: 100vh;
-      background-color: ${colors.dashboardBg};
-      align-items: center;
-      padding: 2em;
-    `,
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    min-height: 100vh;
+    background-color: ${colors.dashboardBg};
+    align-items: center;
+    padding: 2em;
+  `,
   homeButton: css`
-      width: 180px;
-      justify-content: center;
-      margin-top: ${spacing.lg};
+    width: 180px;
+    justify-content: center;
+    margin-top: ${spacing.lg};
 
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-    `,
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+  `,
   logo: css`
-      width: 100%;
-      min-width: 250px;
-      max-width: 600px;
-    `,
+    width: 100%;
+    min-width: 250px;
+    max-width: 600px;
+  `,
 });
