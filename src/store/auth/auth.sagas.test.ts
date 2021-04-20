704d21eac271339bbbac70c41708ff10a8efb978
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-04-20 23:26:49 +0200

    SAAS-490 update tests

--- src/store/auth/auth.sagas.test.ts
+++ src/store/auth/auth.sagas.test.ts
@@ -39,6 +39,8 @@ import {
 const TEST_EMAIL = 'test@test.test';
 const TEST_MESSAGE = 'test';
 const TEST_PASSWORD = 'test';
+const TEST_FIRST_NAME = 'Firstname';
+const TEST_LAST_NAME = 'Lastname';
 
 type Action = PayloadAction<string, any>;
 
@@ -324,5 +326,64 @@ describe('Auth Sagas', () => {
     expect(toastError).toBeCalledWith(Messages.genericAPIFailure);
     expect(consoleError).toBeCalledTimes(1);
     expect(consoleError).toBeCalledWith(TEST_MESSAGE);
+  });
+
+  test('authUpdateProfileRequest succeeds', async () => {
+    const updateProfile = jest.spyOn(authApi, 'updateProfile').mockImplementation(() => Promise.resolve({} as AuthPB.UpdateProfileResponse));
+
+    await runSagaPromise(authUpdateProfileRequest as Saga, {
+      firstName: TEST_FIRST_NAME,
+      lastName: TEST_LAST_NAME,
+    });
+
+    expect(dispatchedActions).toEqual([authUpdateProfileAction.success()]);
+
+    expect(updateProfile).toHaveBeenCalledWith({ firstName: TEST_FIRST_NAME, lastName: TEST_LAST_NAME });
+    expect(updateProfile).toHaveBeenCalledTimes(1);
+
+    updateProfile.mockRestore();
+  });
+
+  test('authUpdateProfileRequest fails', async () => {
+    const error = { code: grpcWeb.StatusCode.CANCELLED };
+    const updateProfile = jest.spyOn(authApi, 'updateProfile').mockImplementation(() => Promise.reject(error));
+
+    await runSagaPromise(authUpdateProfileRequest as Saga, { email: TEST_EMAIL });
+
+    expect(dispatchedActions).toEqual([authUpdateProfileAction.failure(error as grpcWeb.Error)]);
+
+    expect(updateProfile).toHaveBeenCalledTimes(1);
+
+    updateProfile.mockRestore();
+  });
+
+  test('authUpdateProfileFailure (invalid argument)', async () => {
+    await runSagaPromise(
+      authUpdateProfileFailure as Saga,
+      { code: grpcWeb.StatusCode.INVALID_ARGUMENT, message: TEST_MESSAGE },
+    );
+
+    expect(dispatchedActions).toEqual([]);
+    expect(toastError).toHaveBeenCalledWith(TEST_MESSAGE);
+    expect(toastError).toHaveBeenCalledTimes(1);
+  });
+
+  test('authUpdateProfileFailure (generic error)', async () => {
+    const error = { code: grpcWeb.StatusCode.CANCELLED };
+
+    await runSagaPromise(authUpdateProfileFailure as Saga, { code: grpcWeb.StatusCode.CANCELLED });
+
+    expect(dispatchedActions).toEqual([]);
+    expect(toastError).toHaveBeenCalledWith(Messages.genericAPIFailure);
+    expect(toastError).toHaveBeenCalledTimes(1);
+    expect(consoleError).toHaveBeenCalledTimes(1);
+    expect(consoleError).toHaveBeenCalledWith(error);
+  });
+
+  test('authUpdateProfileSuccess', async () => {
+    await runSagaPromise(authUpdateProfileSuccess as Saga);
+
+    expect(toastSuccess).toBeCalledTimes(1);
+    expect(toastSuccess).toBeCalledWith(Messages.updateProfileSucceeded);
   });
 });
