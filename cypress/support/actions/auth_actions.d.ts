2b39a4a2457ef52c0d770863737b0245e24d33d8
yurkovychv <vasyl.yurkovych@percona.com>
2021-05-13 18:50:47 +0300

    SAAS-542 updated actions description

--- cypress/support/actions/auth_actions.d.ts
+++ cypress/support/actions/auth_actions.d.ts
@@ -4,7 +4,26 @@ import { User } from 'pages/common/interfaces/Auth';
 
 declare namespace Cypress {
   interface Chainable {
+    /**
+     * Login as user. (without UI interaction)
+     * If shouldFail is true it doesn't refresh page. (useful when we want to verify a pop up message)
+     *
+     * @example
+     *   const user = EXISTING_USER;
+     *   cy.runLoginFlow(user);
+     * @param user
+     * @param shouldFail
+     */
     runLoginAction(user: User, shouldFail: boolean): Chainable;
+
+    /**
+     * Sign up a user. (without UI interaction)
+     *
+     * @example
+     *   const user = getUser();
+     *   cy.runSignUpAction(user);
+     * @param user
+     */
     runSignUpAction(user: User): Chainable;
   }
 }
