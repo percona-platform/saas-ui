c37977b97d9ae9738205d0e7ae61945ce3eed69d
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2021-07-16 10:48:41 +0200

    SAAS-544 fix e2e test

--- cypress/pages/profile/constants.ts
+++ cypress/pages/profile/constants.ts
@@ -1,4 +1,4 @@
-export const OKTA_PROFILE_SETTINGS = 'https://okta.percona.com/enduser/settings';
+export const OKTA_PROFILE_SETTINGS = 'https://okta-dev.percona.com/enduser/settings';
 export const MESSAGES = {
   PROFILE_UPDATED: 'Your profile information has been saved',
   TO_LONG_STRING: 'Must contain at most 50 characters',
