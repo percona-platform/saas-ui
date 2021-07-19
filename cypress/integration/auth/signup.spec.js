d5c17028a5f5b1b2305780ec4df5197618494e25
Vasyl Yurkovych <59879559+yurkovychv@users.noreply.github.com>
2021-07-19 19:16:17 +0300

    Update signup.spec.js

--- cypress/integration/auth/signup.spec.js
+++ cypress/integration/auth/signup.spec.js
@@ -103,10 +103,6 @@ context('Sign Up', () => {
           cy.get('[name="verifyPassword"]').type(userPassword);
           cy.get('#next-button').click();
           loginForm().isVisible();
-          // temp fix until post registration steps are defined
-          // cy.get('[data-se="org-logo"]').isVisible();
-          // cy.get('[data-se="dropdown-menu-button-header"]').isVisible();
-          // cy.url().should('include', '/app/UserHome');
         });
     });
   });
